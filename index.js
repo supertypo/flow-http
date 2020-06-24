3a1c9e03f87dba9056ed6439c441bdd9c07d6b3b
surinder singh <surinder83singh@gmail.com>
2020-06-24 10:26:42 +0530

    http-app-->flow--http

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const HttpApp = require("./lib/http-app");
+const FlowHttp = require("./lib/flow-http");
 
 module.exports = (modules)=>{
 	let {express} = modules||{};
 	if(typeof express!= 'function')
-		throw new Error("flow-http.HttpApp requires express module.");
-	HttpApp.modules = modules;
-	return {HttpApp}
+		throw new Error("flow-http.FlowHttp requires express module.");
+	FlowHttp.modules = modules;
+	return {FlowHttp}
 }
